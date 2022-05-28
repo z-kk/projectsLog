120ee90e8d00c056aafaaf7abd1c4684c64cb2d1
z-kk <z-kk@gmail.com>
2022-05-28 15:06:30 +0900

    設定読み込み

--- src/projectsLog.nim
+++ src/projectsLog.nim
@@ -1,5 +1,34 @@
 import
-  projectsLogpkg / httpServer
+  os, json,
+  projectsLogpkg / [httpServer, consts]
+
+proc getPort(): int =
+  ## サーバを起動するポートを取得
+  let conf = ConfFile.parseFile
+  if "port" in conf:
+    return conf["port"].getInt
+
+proc makeConfFile() =
+  ## 設定ファイルを作成
+  let conf = %*{
+    "port": 5000,
+    "projects": {
+      "projectName1": {
+        "id": 1,
+        "code": "projectCode",
+      },
+      "projectName2": {
+        "id": 2,
+        "code": "projectCode",
+      },
+    },
+  }
+  ConfFile.writeFile(conf.pretty)
+  echo "設定ファイル[conf.json]を記入してください"
 
 when isMainModule:
-  startHttpServer()
+  if not ConfFile.fileExists:
+    makeConfFile()
+    quit()
+  let port = getPort()
+  startHttpServer(port)
